9e633812cda0bf3262bd4b6deb2d8a0f9cad324f
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2021-05-02 12:59:04 -0400

    partially done the index.js

--- index.js
+++ index.js
@@ -3,3 +3,43 @@ const inquirer = require('inquirer');
 const Manager = require('./library/Manager');
 const Engineer = require('./library/Engineer');
 const Intern = require('./library/Intern');
+const generatePage = require('./src/template');
+const {writeFile, copyFile } = require('./util/generate-website');
+
+const teamMembers = [];
+
+const startApp = () => {
+
+    //adding manager
+    const addManager = () => {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'managerName',
+                messaage: 'What is the group managers name?',
+                validate: answerInput => {
+                    if (answerInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the group managers name.');
+                        return false;
+                    }
+                }
+            },{
+                type:'input',
+                name: 'id',
+                message: 'What is the Managers ID',
+                validate: answerInput => {
+                    if (answerInput) {
+                        return  true;
+                    } else {
+                        console.log('Please enter the Manager ID please.');
+                        return false;
+                    }
+                }
+            },{
+                
+            }
+        ])
+    }
+}
